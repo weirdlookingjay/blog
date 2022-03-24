2693a61f8407511e84669aa5c0863e65e5dc1f87
Joseph Clark <weirdlookingjay@live.com>
2022-03-24 16:49:02 -0400

    checking database connection

--- backend/init.php
+++ backend/init.php
@@ -7,6 +7,6 @@ spl_autoload_register(function($class){
 define("DB_HOST", getenv('DB_HOST'));
 define("DB_NAME", getenv('DB_NAME'));
 define("DB_USER", getenv('DB_USER'));
-define("DB_PASS", getenv('DB_PASS'));
+define("DB_PASS", getenv('DB_PASS1'));
 define('DB_ENCODING', getenv('DB_ENCODING'));
 define("BASE_URL", getenv('BASE_URL'));
