f9c98a9420fad4aa55d1b77a565f423987dc8c3d
Joseph Clark <weirdlookingjay@live.com>
2022-03-24 16:34:30 -0400

    updated env

--- backend/init.php
+++ backend/init.php
@@ -3,7 +3,8 @@ session_start();
 spl_autoload_register(function($class){
    require 'classes/'.$class.'.php';
 });
-define("DB_HOST","localhost");
+// define("DB_HOST","localhost");
+define("DB_HOST",getenv('greeting2'));
 define("DB_NAME","blogger");
 define("DB_USER","root");
 define("DB_PASS","r00tadmin");
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 include 'backend/init.php';
 
 // $greeting = getenv('greeting');
-define("DB_HOST",getenv('greeting2'));
+
 echo DB_HOST;
 
 
